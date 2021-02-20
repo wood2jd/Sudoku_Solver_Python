28ea50ed622e2f5d3365f961158806a3de5631e2
Jon Wood <wood3jd@gmail.com>
2021-02-20 16:18:46 -0500

    Initial commit

---
+++ Sudoku_Solver_Python.py
@@ -0,0 +1,55 @@
+import random
+
+##Sudoku Solve in Python
+##Coding Practice
+
+def CheckValidSolution():
+	##Verifies that the solution to the sudoku
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
